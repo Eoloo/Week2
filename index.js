de6c7b303e8e259ad348f5cdf03124c1357033bb
Eoloo <evans.oloo@student.moringaschool.com>
2022-10-21 11:37:13 +0300

    files added

---
+++ index.js
@@ -0,0 +1,44 @@
+
+//const weekDays = (( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+//const weekDay = 
+
+//TO DO
+//create date and month variables
+//create a function and pass an arguement
+
+//document.getElementById("count-el").innerText = 5
+//let countEl = document.getElementById("count-el")
+// 
+
+const MyBirthday = () =>{
+const dateElement = document.getElementById("Date")
+const monthElement= document.getElementById("Month/Year")
+const genderElement = document.getElementById("Gender")
+const myBirthdayElement = document.getElementById("My Birthday!!")
+
+const Date = fNameElement.value;
+const Month = lNameElement.value;
+const Gender = emailElement.value;
+
+   //do some checks on the inputs to ensure user enters all required data
+
+if (Date == '') {
+    alert('Please enter your date of birth')
+}
+if (Month == '') {
+    alert('Please enter your Month and Year of birth')
+}
+if (Gender == '') {
+    alert('Please enter your Gender')
+}
+
+else {
+
+    alert('Congratulations, you are Akan')
+
+}
+      
+
+console.log(MyBirthday)
+
+}
