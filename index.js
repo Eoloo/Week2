c01542611c0f1bf9f0e5343c97c47f40fed3045d
Eoloo <evans.oloo@student.moringaschool.com>
2022-10-24 09:58:18 +0300

    script updated

--- index.js
+++ index.js
@@ -1,57 +1,109 @@
 
-//const weekDays = (( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-//const weekDay = 
-
-//TO DO
-//create date and month variables
-//create a function and pass an arguement
-
-//document.getElementById("count-el").innerText = 5
-//let countEl = document.getElementById("count-el")
-// 
-
 const myBirthday = () => {
-const dateElement = document.getElementById("date")
-const genderElement = document.getElementById("gender")
+   // retrieve elements
+   var dateElement = document.getElementById("date")
+   const genderElement = document.getElementById("gender")
+   
+   
+   // retrieving input values
+   
+   var date = dateElement.value; 
+   const gender = genderElement.value;
 
-
-
-
-const date = dateElement.value;
-const gender = genderElement.value;
-const myBirthday = [date,gender];
-
-console.log(myBirthday) 
-
-//setting date 
-
-
-
-   //do some checks on the inputs to ensure user enters all required data
-
-// if (date == '') {
-//     alert('Please enter your date of birth')
-// }
-
-// if (month == '') {
-//     alert('Please enter your Month and Year of birth')
-// }
-
-// if (gender == '') {
-//     alert('Please enter your Gender')
-// }
-// else {
-
-//     alert('Congratulations, you are Akan')
-
-// }  
-// console.log(myBirthday) 
-// }
-}                                                  
-//GETTING THE DAYS OF THE WEEK CONSTANT
-// const Weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-// let weekday = Weekdays [document.getWeekdays()];
-// 
-//GETTING THE DAYS OF THE MONTH CONSTANT
-// const Monthsdays = ["d<31"]
-// let Monthday = monthsday [document.getMonthsDay()];
+   const myBirthday = [date,gender]
+   
+   // day of the week function
+   // Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+   // date = cc-yy-mm-dd
+   // Retrieve the date values from the date of birth for us to run the day of the week function
+   // convert date values to number
+   
+      
+   var CC = Number(date.slice(0,2));
+   
+   var YY = Number (date.slice(2,4));
+   
+   var MM = Number (date.slice(5,7));
+   
+   var DD = Number (date.slice(8,10));
+   
+   //     const dates = [CC, YY, MM, DD]
+   
+   // console.log(date)
+   // 
+   
+   
+   var dayOfTheWeek = Math.floor((((CC/4)-2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD)%7);
+       
+       
+   //    days of the week
+   
+   const daysOfTheWeek = [
+           "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+   ];
+    
+       //   male Akan names
+   const maleNames = [
+           "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+   ];
+   
+   // female Akan names
+   const femaleNames = [
+           "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+   ];
+   
+   
+         // date of birth validation
+   
+   if (date == '') {
+           alert('please enter a valid date of birth')
+   }
+     
+   if (gender == 'null'){
+             alert('please select your gender')
+   
+   }
+    
+   
+       //   male name get function
+   
+      
+   
+   var index;
+       
+      if (dayOfTheWeek == 0){
+         index = 6;
+      }
+      else {
+         index = dayOfTheWeek - 1;
+      }
+            
+   if (gender == "male" & date != '') {
+      document.getElementById("results").textContent = ('You were born on a ' + daysOfTheWeek[index] + ',' + 'Your Akan Name is ' + maleNames[index] +'!!!')
+                 
+   } 
+   
+   
+   else if (gender == "female" & date != ''){
+      document.getElementById("results").textContent = ('You were born on a ' + daysOfTheWeek[index] + ',' + 'Your Akan Name is ' + femaleNames[index] + '!!!')
+       
+   }
+   
+   else{
+   
+      alert ('You have entered incorrect datails, kindly try again!!')
+   }  
+                   
+   
+   
+           //  let btnClear = document.querySelector('button');
+           //  let inputs = document.querySelectorAll('input');
+           //  btnClear.addEventListener('click', () =>{
+           //   inputs.forEach(input  => input.value = '');
+           //  }); 
+   
+   console.log(myBirthday)
+   
+   
+   }
+               
